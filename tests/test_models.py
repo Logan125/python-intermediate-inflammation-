643950e2acd3035f8cb4cc48cd7d3699489d8953
Shan Tan-Ya <shan.tan-ya@ukaea.uk>
2022-01-26 15:13:47 +0000

    Add initial test cases for daily_max() and daily_min()

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 
 def test_daily_mean_zeros():
@@ -30,4 +31,21 @@ def test_daily_mean_integers():
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 
+def test_daily_max():
+    """Test that max function works for an array of positive integers."""
+    from inflammation.models import daily_max
+    test_input = np.array([[0, 0],
+                           [2, 4],
+                           [3, 2]])
+    test_result = np.array([3, 4])
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
 # TODO(lesson-robust) Implement tests for the other statistical functions
